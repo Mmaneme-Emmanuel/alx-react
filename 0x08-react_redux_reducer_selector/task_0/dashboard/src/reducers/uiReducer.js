736ca3175e12b2a15af2c4d4e28be25d04d133cb
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-12-03 00:12:16 +0100

    task done

--- 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -1,50 +1,30 @@
-// Import action types
-import uiActionTypes from '../actions/uiActionTypes';
+import {
+	DISPLAY_NOTIFICATION_DRAWER,
+	HIDE_NOTIFICATION_DRAWER,
+	LOGIN_SUCCESS,
+	LOGIN_FAILURE,
+	LOGOUT,
+} from '../actions/uiActionTypes';
 
-// Initial state of the UI reducer
-const initialState = {
-  isNotificationDrawerVisible: false,
-  isUserLoggedIn: false,
-  user: {}
+const defaultState = {
+	isNotificationDrawerVisible: false,
+	isUserLoggedIn: false,
+	user: {},
 };
 
-// Reducer function
-const uiReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case uiActionTypes.DISPLAY_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: true,
-      };
-
-    case uiActionTypes.HIDE_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: false,
-      };
-
-    case uiActionTypes.LOGIN_SUCCESS:
-      return {
-        ...state,
-        isUserLoggedIn: true,
-      };
-
-    case uiActionTypes.LOGIN_FAILURE:
-      return {
-        ...state,
-        isUserLoggedIn: false,
-      };
-
-    case uiActionTypes.LOGOUT:
-      return {
-        ...state,
-        isUserLoggedIn: false,
-      };
-
-    default:
-      return state; // Return the current state if no action matches
-  }
-};
-
-// Export the initial state and the reducer
-export { initialState, uiReducer };
+export default function uiReducer(state = defaultState, action) {
+	switch (action.type) {
+		case DISPLAY_NOTIFICATION_DRAWER:
+			return { ...state, isNotificationDrawerVisible: true };
+		case HIDE_NOTIFICATION_DRAWER:
+			return { ...state, isNotificationDrawerVisible: false };
+		case LOGIN_SUCCESS:
+			return { ...state, isUserLoggedIn: true };
+		case LOGIN_FAILURE:
+			return { ...state, isUserLoggedIn: false };
+		case LOGOUT:
+			return { ...state, isUserLoggedIn: false };
+		default:
+			return state;
+	}
+}
